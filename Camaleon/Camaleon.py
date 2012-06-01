5d33a7769eae9bbe82cf23d26bcd21968b7894a7
Staicu Ionut <github@iamntz.com>
2012-06-01 21:28:33 +0300

    added smart completion for many css snippets; various updates & changes

---
+++ Camaleon/Camaleon.py
@@ -0,0 +1,90 @@
+# coding=utf8
+import sublime, sublime_plugin
+import os
+
+class CamaleonCommand(sublime_plugin.WindowCommand):
+	def run(self, type = 'next'):
+
+		s = sublime.load_settings('Camaleon.sublime-settings')
+		current = int(s.get('current'))
+
+		try:
+			s.get('camaleon')[current][0]
+		except:
+			current = 0
+			try:
+				s.get('camaleon')[current][0]
+			except:
+				return # total empty
+		try:
+			if type == 'next':
+				s.get('camaleon')[current+1][0]
+				next = current+1
+			else:
+				s.get('camaleon')[current-1][0]
+				next = current-1
+				if next < 0:
+					next = len(s.get('camaleon'))-1
+		except:
+			try:
+				if type == 'next':
+					next = 0
+				else:
+					next = len(s.get('camaleon'))-1
+				s.get('camaleon')[next][0]
+			except:
+				return # total empty
+
+		# chrome change
+
+		#check for soda theme if we are going to switch to soda
+		if s.get('camaleon')[next][0].find('Soda') == 0 and False == os.path.isdir(os.path.join(sublime.packages_path(), 'Theme - Soda')):
+			pass
+		else:
+			if int(sublime.version()) >= 2174:
+				sublime_s = sublime.load_settings('Preferences.sublime-settings')
+			else:
+				sublime_s = sublime.load_settings('Global.sublime-settings')
+			sublime_s.set('theme', s.get('camaleon')[next][0]);
+			if int(sublime.version()) >= 2174:
+				sublime.save_settings('Preferences.sublime-settings')
+			else:
+				sublime.save_settings('Global.sublime-settings')
+
+		# colour scheme change
+		if int(sublime.version()) >= 2174:
+			sublime_s = sublime.load_settings('Preferences.sublime-settings')
+		else:
+			sublime_s = sublime.load_settings('Base File.sublime-settings')
+
+		sublime_s.set('color_scheme', s.get('camaleon')[next][1]);
+		if int(sublime.version()) >= 2174:
+			sublime.save_settings('Preferences.sublime-settings')
+		else:
+			sublime.save_settings('Base File.sublime-settings')
+
+		s.set('current', next);
+		sublime.save_settings('Camaleon.sublime-settings')
+
+class CamaleonRandomColourSchemeCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		schemes = []
+		for scheme in os.listdir(os.path.join(sublime.packages_path(), 'Color Scheme - Default')):
+			if scheme[-7:] == 'tmTheme':
+				schemes.append(scheme)
+		from random import choice
+		scheme = choice(schemes)
+		if scheme != '':
+			if int(sublime.version()) >= 2174:
+				sublime_s = sublime.load_settings('Preferences.sublime-settings')
+			else:
+				sublime_s = sublime.load_settings('Base File.sublime-settings')
+
+			sublime_s.set('color_scheme', "Packages/Color Scheme - Default/"+scheme);
+
+			if int(sublime.version()) >= 2174:
+				sublime.save_settings('Preferences.sublime-settings')
+			else:
+				sublime.save_settings('Base File.sublime-settings')
+
+			sublime.status_message(u'Camaleon : Loaded colour scheme : '+scheme.decode('utf-8'))
---
+++ Camaleon/readme.md
@@ -0,0 +1,38 @@
+Description
+------------------
+
+Camaleón plugin allows you to quickly cycle between a combination of user defined chrome themes and colours schemes when pressing F8
+
+By pressing CTRL+F8 a random colour scheme will be chosen
+
+Installation
+------------------
+
+Install this package via "Package Control" see: http://wbond.net/sublime_packages/package_control
+
+Usage
+------------------
+
+By pressing F8 you will automatically cycle between your colour choices.
+
+Customization of combinations can be done by editing "Camaleon.sublime-settings"
+
+To go to the previous group press SHIFT+F8
+
+This plugin recommends "Soda" theme. See: https://github.com/buymeasoda/soda-theme
+
+Source / Installation
+------------------
+
+https://github.com/SublimeText/Camaleon
+
+Forum Thread
+------------------
+
+http://www.sublimetext.com/forum/viewtopic.php?f=5&t=4435
+
+
+License
+------------------
+
+See license.txt
